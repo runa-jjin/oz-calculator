a03e46bb193d8d8625d2ebab7751961079162f73
runa-jjin <tears0427@gmail.com>
2025-07-21 20:21:14 +0900

    Add exponent operation (^) to calculator

--- js/input.js
+++ js/input.js
@@ -3,7 +3,7 @@
 
 // Costanti per validazione
 const VALID_NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const VALID_OPERATORS = ["+", "-", "*", "/", "^"]; // Include ^ per il compito avanzato
+const VALID_OPERATORS = ["+", "-", "*", "/", "^"]; // Added ^ for exponent - feature-exponent
 
 /**
  * Resetta il display a vuoto
